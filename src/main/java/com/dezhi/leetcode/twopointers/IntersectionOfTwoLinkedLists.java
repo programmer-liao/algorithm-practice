6cdaed208b4014d883e902634ff396500e86bf9b
liaodezhi <2380167274@qq.com>
2023-03-29 11:31:56 +0800

    feat: leetcode 160.相交链表 [https://leetcode.cn/problems/intersection-of-two-linked-lists/]

---
+++ src/main/java/com/dezhi/leetcode/twopointers/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,30 @@
+package com.dezhi.leetcode.twopointers;
+
+import java.util.HashSet;
+
+public class IntersectionOfTwoLinkedLists {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        HashSet<ListNode> set = new HashSet<>();
+        while (headA != null) {
+            set.add(headA);
+            headA = headA.next;
+        }
+        while (headB != null) {
+            if (set.contains(headB)) {
+                return headB;
+            }
+            headB = headB.next;
+        }
+        return null;
+    }
+}
