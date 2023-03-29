fcba2b5ed02a8a94ed5ab8a12c5eb3bfe36ef6f2
liaodezhi <2380167274@qq.com>
2023-03-29 10:58:14 +0800

    feat: leetcode 206.反转链表 [https://leetcode.cn/problems/reverse-linked-list/]

---
+++ src/main/java/com/dezhi/leetcode/twopointers/ReverseLinkedList.java
@@ -0,0 +1,37 @@
+package com.dezhi.leetcode.twopointers;
+
+
+public class ReverseLinkedList {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode pre = head;
+        ListNode cur = head.next;
+        pre.next = null;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+}
