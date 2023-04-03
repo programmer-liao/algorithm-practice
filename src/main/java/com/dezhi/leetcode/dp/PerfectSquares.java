5a1a7e36f50cbf5b95d35753a06c1ed66f639113
liaodezhi <2380167274@qq.com>
2023-04-03 19:26:17 +0800

    feat: leetcode 279.完全平方数 [https://leetcode.cn/problems/perfect-squares/]

---
+++ src/main/java/com/dezhi/leetcode/dp/PerfectSquares.java
@@ -0,0 +1,18 @@
+package com.dezhi.leetcode.dp;
+
+import java.util.Arrays;
+
+public class PerfectSquares {
+
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 0; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i - j * j] + 1, dp[i]);
+            }
+        }
+        return dp[n];
+    }
+}
